cff04f918ea92c53298a41611c6729c5a1f55561
Pierre Bailly <pierre.bailly@2dcom.fr>
2022-06-26 14:54:27 -0700

    Ajout d'un dark mode maison

--- assets/app.js
+++ assets/app.js
@@ -11,4 +11,16 @@ import './styles/app.scss';
 // start the Stimulus application
 import './bootstrap';
 
+let checkBoxDarkMode = document.getElementById('dn');
+console.log("hello");
+checkBoxDarkMode.addEventListener('change', function() {
+    if (this.checked) {
+        console.log("coucou");
+        document.body.style.backgroundImage = "url('/build/images/darkback.e0d9adbe.jpg')";
+        document.body.style.color = "#FFFFFF"
+    } else {
+        document.body.style.backgroundImage = "url('/build/images/Scene-7.e856f364.jpg')";
+        document.body.style.color = "#000000"
+    }
+});
 
